a89e4079a99f89332e37f1fff654569f49ac30eb
KylieTKay <113371037+KylieTKay@users.noreply.github.com>
2023-09-15 09:39:50 -0400

    merged css
    index needs to be made responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 
 
+(function(){
+	"use strict";
+	
+	console.log("fired");
+
+	let button = document.querySelector("#hamburger");
+	let burgerCon = document.querySelector("#burger-con");
+
+	function hamburgerMenu() {
+		burgerCon.classList.toggle("slide-toggle");
+		button.classList.toggle("expanded");
+	};
+
+	button.addEventListener("click", hamburgerMenu, false);
+		
+})();
+
+
 
 
 // // let theButtons = document.querySelectorAll;
@@ -40,6 +58,7 @@ let team = {
 	//#
 	//#red
 	moh: {
+		profileImageSrc: 'assets/desktop_images/team_images/taylor_khan.jpg',
 		name: "MOH ZARANDAH",
 		Role: "DEVELOPEMENT/DESIGN",
 		Description: "My role in this project included creating the video's intro logo and developing the website. I designed the logo for the video and handled the HTML, CSS, and JavaScript coding for the website, ensuring responsiveness and proper semantic tagging.",
@@ -47,12 +66,14 @@ let team = {
 	//#
 	//#yellow
 	mirdan: {
+		profileImageSrc: 'assets/desktop_images/team_images/taylor_khan.jpg',
 		name: "MIRDAN ASSI",
 		Role: "RESEARCH/CONTENT CREATOR",
 		Description: "In this project, I conducted research, curated content, selected images, and did some coding to create an informative and visually appealing website for Fanshawe's Interactive Media Design program and Level 4 students' portfolios.",
 	},	//#
 	//#green
 	jason: {
+		profileImageSrc: 'assets/desktop_images/team_images/taylor_khan.jpg',
 		name: "JASON DIFUNTORUM",
 		Role: "VIDEOGRAPHER",
 		Description: "My primary role in this project was to create a compelling video that showcased the project's concept using visuals, including images and short videos, accompanied by music. I also assisted in selecting visuals for the website and contributed to some coding tasks to ensure a seamless user experience.",
@@ -181,7 +202,77 @@ function taylorLB() {
 closeBtn.addEventListener('click', () => {
     popUpCon.classList.add('hidden');
 });
+//#
 
+//#red
+function mohLB() {
+    console.log("Pop-Up");
+
+	// let profileImageSrc = 'assets/desktop_images/team_images/moh_khan.jpg';
+	// previewSrc1 = '';
+	// previewSrc2 = '';
+
+	popUpCon.classList.remove('hidden');
+	role.textContent = team[this.dataset.member].Role;
+	teamName.textContent = team[this.dataset.member].name;
+	profileImg.src =  team[this.dataset.member].profileImageSrc;
+	teamParagraph.textContent =  team[this.dataset.member].Description;
+	// profileImg.src =  team[this.dataset.member].profileImageSrc;
+
+};
+closeBtn.addEventListener('click', () => {
+    popUpCon.classList.add('hidden');
+});
+//#
+
+//#green
+function jasonLB() {
+    console.log("Pop-Up");
+
+	// let profileImageSrc = 'assets/desktop_images/team_images/jason_khan.jpg';
+	// previewSrc1 = '';
+	// previewSrc2 = '';
+
+	popUpCon.classList.remove('hidden');
+	role.textContent = team[this.dataset.member].Role;
+	teamName.textContent = team[this.dataset.member].name;
+	profileImg.src =  team[this.dataset.member].profileImageSrc;
+	teamParagraph.textContent =  team[this.dataset.member].Description;
+	// profileImg.src =  team[this.dataset.member].profileImageSrc;
+
+};
+closeBtn.addEventListener('click', () => {
+    popUpCon.classList.add('hidden');
+});
+//#
+
+
+//#yellow
+function mirdanLB() {
+    console.log("Pop-Up");
+
+	// let profileImageSrc = 'assets/desktop_images/team_images/mirdan_khan.jpg';
+	// previewSrc1 = '';
+	// previewSrc2 = '';
+
+	popUpCon.classList.remove('hidden');
+	role.textContent = team[this.dataset.member].Role;
+	teamName.textContent = team[this.dataset.member].name;
+	profileImg.src =  team[this.dataset.member].profileImageSrc;
+	teamParagraph.textContent =  team[this.dataset.member].Description;
+	// profileImg.src =  team[this.dataset.member].profileImageSrc;
+
+};
+closeBtn.addEventListener('click', () => {
+    popUpCon.classList.add('hidden');
+});
+//#
+
+mirdanButton2.forEach(button => button.addEventListener("click", mirdanLB)),
+
+jasonButton2.forEach(button => button.addEventListener("click", jasonLB)),
+
+mohButton2.forEach(button => button.addEventListener("click", mohLB)),
 
 taylorButton2.forEach(button => button.addEventListener("click", taylorLB)),
 
@@ -189,22 +280,5 @@ taylorButton2.forEach(button => button.addEventListener("click", taylorLB)),
 closeBtn.forEach(button => button.addEventListener("click", closeLightBox));
 
 
-//#
 
 
-(function(){
-	"use strict";
-	
-	console.log("fired");
-
-	let button = document.querySelector("#hamburger");
-	let burgerCon = document.querySelector("#burger-con");
-
-	function hamburgerMenu() {
-		burgerCon.classList.toggle("slide-toggle");
-		button.classList.toggle("expanded");
-	};
-
-	button.addEventListener("click", hamburgerMenu, false);
-		
-})();
